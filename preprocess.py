aba206d363566fbffb443608f1b1b518011bd25c
ruxandrailiescu <iliescuruxandra23@stud.ase.ro>
2026-01-12 10:47:00 +0200

    adding a new data file with the play also preprended

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-def preprocess_data(csv_file):
+def preprocess_data(csv_file, out_file):
     """ create a text file which contains all the lines of the dialogue with the player preprended """
     df = pd.read_csv(csv_file)
     df = df[(df['Player'].isna()==False) & (df['ActSceneLine'].to_string().strip()!="")]
     df['PrevPlayer'] = df['Player'].shift(1)
     mask = df['PrevPlayer'].isna() | (df['Player'] != df['PrevPlayer'])
-    df.loc[mask, 'PlayerLine'] = '\n' + df['Player'] + ': ' + df['PlayerLine'].astype(str)
-    with open('alllines_processed.txt', 'w') as f:
+    df.loc[mask, 'PlayerLine'] = '\n' + df['Play'] + ', ' + df['Player'] + ': ' + df['PlayerLine'].astype(str)
+    with open(out_file, 'w') as f:
         for value in df['PlayerLine']:
             if str(value).startswith('\n'):
                 f.write(str(value) + '\n')
